70997e7e036eeebddaa34629cbb9aaa8aef779a6
anju-targetintegration <anju.dhiman@targetintegration.com>
2024-09-25 14:30:15 +0530

    Update custom_invoice.py

--- custom_invoice/models/custom_invoice.py
+++ custom_invoice/models/custom_invoice.py
@@ -10,8 +10,9 @@ class AccountMove(models.Model):
 
     def action_register_payment(self):
         """get payment state and tciekyt ref  and call redmine API function"""
+        result = super(AccountMove, self).action_register_payment()
         for move in self:
-             _logger.info("testing custom invoice")
+             _logger.info("1testing custom invoice")
              _logger.info(move.payment_state)
              _logger.info(move.ref)
              if move.ref:
