176720f061f0c06285f60d0d7de20f44cd113417
anju-targetintegration <anju.dhiman@targetintegration.com>
2024-09-26 10:54:38 +0530

    Update crm_lead.py

--- request_callback/models/crm_lead.py
+++ request_callback/models/crm_lead.py
@@ -1,9 +1,22 @@
-from odoo import models, api
+from odoo import models, api,fields
 import logging
 _logger = logging.getLogger(__name__)
 
 class CRMLead(models.Model):
     _inherit = 'crm.lead'
+
+    lead_properties_valid = fields.Boolean(
+        string='Lead Properties Valid', compute='_compute_lead_properties_valid')
+
+    @api.depends('lead_properties')
+    def _compute_lead_properties_valid(self):
+        for record in self:
+            # Customize this condition based on how you want to evaluate lead_properties
+            if record.lead_properties:
+                # Example condition: Check if the One2many field has at least one entry
+                record.lead_properties_valid = len(record.lead_properties) > 0
+            else:
+                record.lead_properties_valid = False
 
     def request_consultant(self):
         _logger.info(self.phone)
