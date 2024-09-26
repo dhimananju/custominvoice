e9cb93395c012e4df13e28f41c501a5ba9f1cba4
anju-targetintegration <anju.dhiman@targetintegration.com>
2024-09-26 10:43:46 +0530

    Update crm_lead.py

--- request_callback/models/crm_lead.py
+++ request_callback/models/crm_lead.py
@@ -1,7 +1,11 @@
 from odoo import models, api
+import logging
+_logger = logging.getLogger(__name__)
 
 class CRMLead(models.Model):
     _inherit = 'crm.lead'
 
     def request_consultant(self):
+        _logger.info(self.phone)
+        _logger.info(self.lead_properties)
         return "anju here"
