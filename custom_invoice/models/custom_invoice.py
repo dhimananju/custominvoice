99c269a36622c718d76c30bf809043b905bc8881
anju-targetintegration <anju.dhiman@targetintegration.com>
2024-09-26 14:29:36 +0530

    Update custom_invoice.py

--- custom_invoice/models/custom_invoice.py
+++ custom_invoice/models/custom_invoice.py
@@ -9,22 +9,17 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     def action_post(self):
-        _logger.info("hehjzlxklslka")
         # Call the original function to ensure normal behavior
         res = super(AccountMove, self).action_post()
-
         # Call the custom function after posting the invoice
         for invoice in self:
-            _logger.info("jtyutrhfg")
             self.redmine_api(invoice.ref,2)
-
         return res
     
     @api.model
     def button_mark_as_paid(self):
         # Call the super method to ensure the base functionality is executed
         res = super(AccountMove, self).button_mark_as_paid()
-        
         for move in self:
              _logger.info("testing custom invoice")
              _logger.info(move.payment_state)
@@ -32,7 +27,8 @@ class AccountMove(models.Model):
              if move.ref:
                 _logger.info("Change status to in progress")
                 if move.payment_state != "paid":
-                    self.redmine_api(move.ref,2)
+                    text = "Invoice has been raised"
+                    self.redmine_api(move.ref,2,text)
 
         return res
         
@@ -46,14 +42,15 @@ class AccountMove(models.Model):
              if move.ref:
                 _logger.info("testing custom invoice12333")
                 if move.payment_state == "paid":
-                    self.redmine_api(move.ref,5)
+                    text = "This Invoice is paid so closing the ticket"
+                    self.redmine_api(move.ref,5,text)
                  
                  
     """custom function to hit redmine API"""         
-    def redmine_api(self,ref,status):
+    def redmine_api(self,ref,status,text):
         _logger.info("testing custom invoice2")
         redmineurl = "https://mypmstudio.com/issues/"+ref+".json"
-        dataparams = { "issue": { "is_private": "1", "notes": "This Invoice is paid so closing the ticket", "status_id": status } }
+        dataparams = { "issue": {"notes": text, "status_id": status } }
         headers = {"Content-Type": "application/json", "Accept": "application/json", "Catch-Control": "no-cache","X-Redmine-API-Key":"eaffd4f722364a677d97e3e775eacfafc8adca82"}
         response = requests.put(redmineurl, data=json.dumps(dataparams), headers=headers)
         
