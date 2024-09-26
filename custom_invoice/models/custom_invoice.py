014c43ee16c3f99047182057151e899d4384b448
anju-targetintegration <anju.dhiman@targetintegration.com>
2024-09-26 14:05:42 +0530

    Update custom_invoice.py

--- custom_invoice/models/custom_invoice.py
+++ custom_invoice/models/custom_invoice.py
@@ -8,6 +8,16 @@ _logger = logging.getLogger(__name__)
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
+     def action_register_payment(self):
+        """get payment state and tciekyt ref  and call redmine API function"""
+        for move in self:
+             _logger.info("testing custom invoice")
+             _logger.info(move.payment_state)
+             _logger.info(move.ref)
+             if move.ref:
+                _logger.info("Change status to in progress")
+                if move.payment_state != "paid":
+                    self.redmine_api(move.ref,2)
     
     def action_invoice_paid(self):
         """get payment state and tciekyt ref  and call redmine API function"""
@@ -19,14 +29,14 @@ class AccountMove(models.Model):
              if move.ref:
                 _logger.info("testing custom invoice12333")
                 if move.payment_state == "paid":
-                    self.redmine_api(move.ref)
+                    self.redmine_api(move.ref,5)
                  
                  
     """custom function to hit redmine API"""         
-    def redmine_api(self,ref):
+    def redmine_api(self,ref,status):
         _logger.info("testing custom invoice2")
         redmineurl = "https://mypmstudio.com/issues/"+ref+".json"
-        dataparams = { "issue": { "is_private": "1", "notes": "This Invoice is paid so closing the ticket", "status_id": 5 } }
+        dataparams = { "issue": { "is_private": "1", "notes": "This Invoice is paid so closing the ticket", "status_id": status } }
         headers = {"Content-Type": "application/json", "Accept": "application/json", "Catch-Control": "no-cache","X-Redmine-API-Key":"eaffd4f722364a677d97e3e775eacfafc8adca82"}
         response = requests.put(redmineurl, data=json.dumps(dataparams), headers=headers)
         
