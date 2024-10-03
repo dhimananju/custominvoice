a6a1d68a2748b0eeefcf0f383dddce856aade2d1
anju-targetintegration <anju.dhiman@targetintegration.com>
2024-10-03 14:08:26 +0530

    Update custom_invoice.py

--- custom_invoice/models/custom_invoice.py
+++ custom_invoice/models/custom_invoice.py
@@ -14,22 +14,22 @@ class AccountMove(models.Model):
              _logger.info("override _invoice_paid_hook")
              _logger.info(move.payment_state)
              if move.payment_state == "paid":
-                    text = "This Invoice is paid so closing the ticket"
-                    self.redmine_api(move.ref,5,text)
+                 if move.ref!="":
+                        text = "This Invoice is paid so closing the ticket"
+                        self.redmine_api(move.ref,5,text)
     
     def action_post(self):
         # Call the original function to ensure normal behavior
         res = super(AccountMove, self).action_post()
         # Call the custom function after posting the invoice
         for invoice in self:
-            text = "Invoice has been raised"
-            self.redmine_api(invoice.ref,2,text)
+            if invoice.ref!="":
+                text = "Invoice has been raised"
+                self.redmine_api(invoice.ref,2,text)
                 
     """custom function to hit redmine API"""         
     def redmine_api(self,ref,status,text):
-        _logger.info("testing custom invoice2")
         redmineurl = "https://mypmstudio.com/issues/"+ref+".json"
         dataparams = { "issue": {"notes": text, "status_id": status } }
         headers = {"Content-Type": "application/json", "Accept": "application/json", "Catch-Control": "no-cache","X-Redmine-API-Key":"eaffd4f722364a677d97e3e775eacfafc8adca82"}
         response = requests.put(redmineurl, data=json.dumps(dataparams), headers=headers)
-        
