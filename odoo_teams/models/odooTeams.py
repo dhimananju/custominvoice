a770c6bee10f6cb01cd3399bab8515ff3ec2911e
anju-targetintegration <anju.dhiman@targetintegration.com>
2024-10-07 15:36:13 +0530

    Update odooTeams.py

--- odoo_teams/models/odooTeams.py
+++ odoo_teams/models/odooTeams.py
@@ -21,6 +21,7 @@ class odooTeams(models.Model):
         companyid = lead.company_id
         #call custom function to send emssage to teams channel
         #add check for compamny id 5, 6, 10, 1 - Channel 19:c4c08d6944614b2e8930905c905e1c68
+        msg = "A new lead/opp "+name+" is added"
         allowed_companies = [1, 5, 6, 10]
         channelId = '19:c4c08d6944614b2e8930905c905e1c68'
         _logger.info(companyid)
@@ -49,25 +50,26 @@ class odooTeams(models.Model):
             #_logger.info(plain_text)
             #call custom function to send emssage to teams channel
             #add check for compamny id 5, 6, 10, 1 - Channel 19:c4c08d6944614b2e8930905c905e1c68
-             allowed_companies = [1, 5, 6, 10]
+            msg = "A new lead/opp "+name+" is edited with description: vals.get('description')"
+            allowed_companies = [1, 5, 6, 10]
             channelId = '19:c4c08d6944614b2e8930905c905e1c68'
             _logger.info(companyid)
             if companyid.id in allowed_companies:
                 _logger.info(companyid.id)
-                self.sendMessageTeams(name,channelId)
+                self.sendMessageTeams(name,channelId,msg)
                 
             allowed_companies_secod = 3
             channelId_Secon = '19:49218707ae584db683da25b254de80d8'
             if companyid.id == allowed_companies_secod:
-                self.sendMessageTeams(name,channelId_Secon)
+                self.sendMessageTeams(name,channelId_Secon,msg)
 
         return lead
     
-    def sendMessageTeams(self,name,channelId):
+    def sendMessageTeams(self,name,channelId,msg):
         _logger.info("sendMessageTeams")
         _logger.info(name)
         redmineurl = "https://mypmstudio.com/NotifyChannel.json"
-        headers = {"Message": "text-message" ,"ChannelName":channelId,"X-Redmine-API-Key":"eaffd4f722364a677d97e3e775eacfafc8adca82"}
+        headers = {"Message": msg ,"ChannelName":channelId,"X-Redmine-API-Key":"eaffd4f722364a677d97e3e775eacfafc8adca82"}
         response = requests.post(redmineurl, headers=headers)
         _logger.info(response.content)
 
