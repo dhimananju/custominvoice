bb4d12da26db4fd7c4dfd3c9408899e54cf9e299
anju-targetintegration <anju.dhiman@targetintegration.com>
2024-10-07 10:30:01 +0530

    Update contact.py

--- contact_software/models/contact.py
+++ contact_software/models/contact.py
@@ -6,9 +6,4 @@ _logger = logging.getLogger(__name__)
 class CRMLeadRedmine(models.Model):
     _inherit = 'res.partner'
 
-    x_sw = fields.Many2one(
-        'contact.software',  # Reference to the custom model
-        string="Software",
-        ondelete='set null',
-    )
-    #software = fields.Char("sss")
+   
