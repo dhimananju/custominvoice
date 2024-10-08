57c00a5b32bc6f59434418428ddc9dcf36523a85
anju.dhiman <anju.dhiman@targetintegration.com>
2024-10-08 17:00:20 +0530

    update

--- contact_software/models/leads.py
+++ contact_software/models/leads.py
@@ -10,3 +10,17 @@ class CRMLeadSoftware(models.Model):
          'contact.software',  # Reference to the custom model
          string="Software",
      )
+    
+    @api.model
+    def create(self, vals):
+        _logger.info("create leads method called")
+        lead = super(odooTeams, self).create(vals)
+        partner_id = lead.partner_id
+        swlist = lead.swlist
+        _logger.info(swlist)
+        #check if partner id exist or not , if exist update software list there
+        #partner_record = request.env['res.partner'].sudo().browse(partner_id.id)
+            #if partner_record.exists():
+                
+
+        return lead
