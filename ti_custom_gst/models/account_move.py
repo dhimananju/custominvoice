991e5f66979c4142f8dfb652f3b4aa7d000938d8
anju-targetintegration <anju.dhiman@targetintegration.com>
2024-12-24 17:45:51 +0530

    Update account_move.py

--- ti_custom_gst/models/account_move.py
+++ ti_custom_gst/models/account_move.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import fields, models,api
 
 class Accountmove(models.Model):
     _inherit = "account.move"
 
     legal_entity_punjab = fields.Boolean(string="Legal Entity Punjab", default=False)  # Checkbox field
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        docs = self.env['account.move'].browse(docids)
+        return {
+            'doc_ids': docids,
+            'doc_model': 'account.move',
+            'docs': docs,
+    }
