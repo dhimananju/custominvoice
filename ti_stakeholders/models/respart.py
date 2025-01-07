008a17fd093249c9dd6cbd35720c2e74916ffa30
anju-targetintegration <anju.dhiman@targetintegration.com>
2025-01-07 17:36:30 +0530

    Update respart.py

--- ti_stakeholders/models/respart.py
+++ ti_stakeholders/models/respart.py
@@ -5,11 +5,6 @@ from odoo import fields, models, _
 class respart(models.Model):
     _inherit = 'res.partner'
 
-    
-    relationship = fields.Selection([
-        ('decision_maker', 'Decision Maker'),
-        ('influencer', 'Influencer')
-    ], string='Relationship', default='decision_maker')
     lead_id = fields.Many2one('crm.lead', string='CRM Lead', ondelete='set null')
 
     
